04ece5c35d5f9853d5b01facb74690d35bd2a178
Karan Singh <devkaran.codes@gmail.com>
2024-11-21 00:34:27 +0530

    Gsap 02 String Effect

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
-gsap.to(".page2 h1", {
-    transform: "translateX(-220%)",
-    scrollTrigger: {
-        trigger: ".page2",
-        scroller: "body",
-        markers: true,
-        start: "top 0",
-        end: "top -200%",
-        scrub: 3,
-        pin: true
-    }
+var initialPath = "M 10 100 Q 500 100 900 100"
+var finalPath = "M 10 100 Q 500 100 900 100"
+
+var string = document.querySelector(".main")
+string.addEventListener("mousemove", function(dets){
+    path = `M 10 100 Q ${dets.x} ${dets.y} 900 100`
+
+    gsap.to("svg path", {
+        attr: {d:path},
+        duration: 0.5,
+        ease: "power3.out"
+    })
+})
+
+string.addEventListener("mouseleave", function(){
+    gsap.to("svg path", {
+        attr: {d:finalPath},
+        duration: 1.5,
+        ease: "elastic.out(1,0.2)"
+    })
 })
