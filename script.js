59dc698d287bddf8dce6f85332e6d237ddf2b04d
Karan Singh <devkaran.codes@gmail.com>
2024-11-22 20:47:20 +0530

    OG cursor Hide + Cursor scale on Img Hover

--- script.js
+++ script.js
@@ -1,25 +1,28 @@
 var main = document.querySelector("#main")
 var cursor = document.querySelector("#cursor")
+var imageDiv = document.querySelector("#image")
 
 main.addEventListener("mousemove", function(dets){
     gsap.to(cursor, {
         x: dets.x,
         y: dets.y,
-        duration: 0.5,
-        ease: "power.out"
     })
 })
 
-gsap.to("#page3 h1", {
-    transform: "translateX(-3600px)",
-    scrollTrigger: {
-        scrub: 3,
-        trigger: "#page3",
-        scroller: "body",
-        markers: true,
-        start: "top 0",
-        end: "top -200%",
-        pin: true
-    },
-    
+image.addEventListener("mouseenter", function(){
+    cursor.innerHTML = "View More"
+    gsap.to(cursor, {
+        scale: 3,
+        backgroundColor: "#34343474",
+        backdropFilter: "blur(5px)"
+    })
+})
+
+image.addEventListener("mouseleave", function(){
+    cursor.innerHTML = " "
+    gsap.to(cursor, {
+        scale: 1,
+        backgroundColor: "orange"
+
+    })
 })
