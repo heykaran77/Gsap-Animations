2351ef19d5eeca1bc1fc5392a2f4c542a1ff1306
Karan Singh <devkaran.codes@gmail.com>
2024-11-23 01:15:34 +0530

    Gsap Text Anim Proj03

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
 var main = document.querySelector("#main")
 var cursor = document.querySelector("#cursor")
-var menu = document.querySelector("#nav i")
-var cross = document.querySelector("#full i")
-var full = document.querySelector("#full")
+
+var h1 = document.querySelector("h1")
+var h1Txt = h1.textContent
+var splittedText = h1Txt.split("")
+var cluttered = ""
+
+var halfValue = Math.floor(splittedText.length/2)
+console.log(halfValue)
+
+splittedText.forEach(function(letter, idx){
+    if(idx<halfValue)
+    {
+        cluttered += `<span class="a">${letter}</span>`
+    }
+    else{
+        cluttered += `<span class="b">${letter}</span>`
+    }
+})
+
+h1.innerHTML = cluttered
+
 main.addEventListener("mousemove", function(dets){
     gsap.to(cursor, {
         x: dets.x,
@@ -10,54 +28,17 @@ main.addEventListener("mousemove", function(dets){
     })
 })
 
-var tl = gsap.timeline()
-
-tl.to("#full", {
-    right: 0,
-    duration: 0.5,
-    ease: "power.in"
-})
-tl.from("#full h4", {
-    x: 150,
+gsap.from("h1 .a", {
+    y: 70,
+    duration: 0.8,
+    delay: 0.5,
     opacity: 0,
-    stagger: 0.15,
+    stagger: 0.15
 })
-tl.from("#full i", {
-    opacity: 0
-})
-tl.pause()
-
-full.addEventListener("mouseenter", function(){
-    gsap.to(cursor, {
-        scale: 3,
-    })
-})
-full.addEventListener("mouseleave", function(){
-    gsap.to(cursor, {
-        scale: 1,
-    })
-})
-menu.addEventListener("mouseenter", function(){
-    cursor.innerHTML = "open"
-    gsap.to(cursor, {
-        scale: 3,
-    })
-})
-menu.addEventListener("mouseleave", function(){
-    cursor.innerHTML = ""
-    gsap.to(cursor, {
-        scale: 1,
-    })
-})
-menu.addEventListener("click",function(){
-    tl.play()
-})
-cross.addEventListener("click", function(){
-    tl.reverse()
-})
-cross.addEventListener("mouseenter", function(){
-    cursor.innerHTML = "close"
-})
-cross.addEventListener("mouseleave", function(){
-    cursor.innerHTML = ""
+gsap.from("h1 .b", {
+    y: 70,
+    duration: 0.8,
+    delay: 0.5,
+    opacity: 0,
+    stagger: -0.15
 })
