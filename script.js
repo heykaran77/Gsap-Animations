b9b0bcfb9bd1e3046ae1dd856a81d42b22472409
Karan Singh <devkaran.codes@gmail.com>
2024-11-23 15:05:22 +0530

    Gsap Mouse wheel Listner + Proj04

--- script.js
+++ script.js
@@ -1,44 +1,34 @@
 var main = document.querySelector("#main")
 var cursor = document.querySelector("#cursor")
-
-var h1 = document.querySelector("h1")
-var h1Txt = h1.textContent
-var splittedText = h1Txt.split("")
-var cluttered = ""
-
-var halfValue = Math.floor(splittedText.length/2)
-console.log(halfValue)
-
-splittedText.forEach(function(letter, idx){
-    if(idx<halfValue)
-    {
-        cluttered += `<span class="a">${letter}</span>`
-    }
-    else{
-        cluttered += `<span class="b">${letter}</span>`
-    }
-})
-
-h1.innerHTML = cluttered
-
 main.addEventListener("mousemove", function(dets){
     gsap.to(cursor, {
         x: dets.x,
         y: dets.y,
+        duration: 0.1
     })
 })
 
-gsap.from("h1 .a", {
-    y: 70,
-    duration: 0.8,
-    delay: 0.5,
-    opacity: 0,
-    stagger: 0.15
-})
-gsap.from("h1 .b", {
-    y: 70,
-    duration: 0.8,
-    delay: 0.5,
-    opacity: 0,
-    stagger: -0.15
+window.addEventListener("wheel", function(dets){
+    if(dets.deltaY>0){
+        gsap.to(".marque", {
+            transform: "translateX(-200%)",
+            duration:3,
+            repeat: -1,
+            ease: "none"
+        })
+
+        gsap.to(".marque img", {
+            rotate: 180
+        })
+    }else{
+        gsap.to(".marque", {
+            transform: "translateX(0%)",
+            duration:3,
+            repeat: -1,
+            ease: "none"
+        })
+        gsap.to(".marque img", {
+            rotate: 0
+        })
+    }
 })
